38e554b83dff894af834588389c082c7bf81276e
codayblue <codayblue@gmail.com>
2022-07-30 08:16:22 -0700

    feat(app): app side is done and should function

--- main.go
+++ main.go
@@ -1,7 +1,78 @@
 package main
 
-import "fmt"
+import (
+	"os"
+	"os/exec"
+	"path"
+
+	"gopkg.in/yaml.v3"
+)
+
+type Repo struct {
+	Name       string
+	Repository string
+}
+
+type Repositories struct {
+	Dependencies []Repo
+}
+
+type HelmCommand struct {
+	helmbin string
+	stdout  *os.File
+	stderr  *os.File
+}
+
+func (helmcommand *HelmCommand) execute(args ...string) {
+	cmd := exec.Command(helmcommand.helmbin, args...)
+
+	cmd.Stdout = helmcommand.stdout
+	cmd.Stderr = helmcommand.stderr
+
+	err := cmd.Run()
+
+	handleError(err)
+}
+
+func handleError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func getDependencies(chartYaml []byte) []Repo {
+	var repos Repositories
+
+	err := yaml.Unmarshal(chartYaml, &repos)
+
+	handleError(err)
+
+	return repos.Dependencies
+}
+
+func addRepos(helmcommand HelmCommand, repos []Repo) {
+	for _, repo := range repos {
+		helmcommand.execute("repo", "add", repo.Name, repo.Repository)
+	}
+}
 
 func main() {
-	fmt.Println("Testing")
+	currentDir, err := os.Getwd()
+
+	handleError(err)
+
+	chartYaml, err := os.ReadFile(path.Join(currentDir, "Chart.yaml"))
+
+	handleError(err)
+
+	repos := getDependencies(chartYaml)
+	helmcommand := HelmCommand{
+		helmbin: os.Getenv("HELM_BIN"),
+		stdout:  os.Stdout,
+		stderr:  os.Stderr,
+	}
+
+	addRepos(helmcommand, repos)
+
+	helmcommand.execute("dependencies", "build")
 }
